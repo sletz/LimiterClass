807aed88fd7c71a8001c12aa67f83445c8a456e7
Stephane Letz <letz@grame.fr>
2022-05-15 07:39:25 +0200

    Simulates noise input.

--- adapterTemplate.cpp
+++ adapterTemplate.cpp
@@ -7,13 +7,16 @@
 struct mydsp : public dsp {
     
     Limiter<float> limiterStereo;
+    FAUSTFLOAT** noise_inputs;
     
     void init(int sample_rate)
     {
         limiterStereo.SetSR(sample_rate);
     }
     
-    int getNumInputs() { return 2; }
+    // Adapted DSP does not have inputs anymore
+    int getNumInputs() { return 0; }
+    
     int getNumOutputs() { return 2; }
     
     int getSampleRate() { return 44100; }
@@ -38,15 +41,65 @@ struct mydsp : public dsp {
     void metadata(Meta* m)
     {}
     
+    mydsp()
+    {
+        noise_inputs = new FAUSTFLOAT*[2];
+        for (int chan = 0; chan < 2; chan++) {
+            noise_inputs[chan] = new FAUSTFLOAT[1024];
+        }
+    }
+    
+    ~mydsp()
+    {
+        for (int chan = 0; chan < 2; chan++) {
+            delete [] noise_inputs[chan];
+        }
+        delete [] noise_inputs;
+    }
+    
+    // For noise generation
+    int32_t state = 0;
+    int32_t seed = 12345;
+    const int32_t MAX = 2147483648 - 1;
+    
     void compute(int count, FAUSTFLOAT** inputs, FAUSTFLOAT** outputs)
     {
-        limiterStereo.SetPreGain(0.0);
+        limiterStereo.SetPreGain(120.0);
         limiterStereo.SetAttTime(0.01);
         limiterStereo.SetHoldTime(0.05);
         limiterStereo.SetRelTime(0.15);
         limiterStereo.SetThreshold(0.0);
+        
+        // Generates noise
+        for (int i = 0; i < count; i++) {
+            state = state * 1103515245 + seed;
+            noise_inputs[0][i] = state / FAUSTFLOAT(MAX);
+            noise_inputs[1][i] = noise_inputs[0][i];
+        }
       
-        limiterStereo.Process(inputs, outputs, count);
+        limiterStereo.Process(noise_inputs, outputs, count);
     }
     
 };
+
+/*
+int main()
+{
+    mydsp DSP;
+    
+    float** inputs = new FAUSTFLOAT*[2];
+    for (int i = 0; i < 2; i++) {
+        inputs[i] = new FAUSTFLOAT[256];
+    }
+    
+    float** outputs = new FAUSTFLOAT*[2];
+    for (int i = 0; i < 2; i++) {
+        outputs[i] = new FAUSTFLOAT[256];
+    }
+    
+    DSP.init(44100);
+    DSP.compute(256, inputs, outputs);
+    
+    return 0;
+}
+*/
